f24a3553777e3812087118299fbb2738af55d896
nhannt112113 <nguyentrongnhan04072000@gmail.com>
2022-12-04 11:44:04 +0700

    Models + Migrations + Seeders

--- src/seeders/seeder-user.js
+++ src/seeders/seeder-user.js
@@ -3,9 +3,14 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
+      email: 'nguyentrongnhan04072000@gmail.com',
+      password: '1234567890',
+      firstName: 'Nguyen',
+      lastName: 'Nhan',
+      address: 'HCMC, Vietnam',
+      gender: 1,  //0: Male || 1: Female
+      typeRole: 'ROLE',
+      keyRole: 'R1',
       createdAt: new Date(),
       updatedAt: new Date()
     }])
